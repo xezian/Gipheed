04e24a74ad8c954c0915d01beaa4e4493ea3b9bb
xezian <xezian@icloud.com>
2017-12-05 15:10:21 -0700

    set up api response

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,24 @@
 // this is an app which will send a request to the GIPHY API based on which button the user pressed to return an assortment of GIFs to to the display for the user to enjoy looking at, and which can be held still or paused by clicking
-var gipheed = {
-    theVars = {
-        queryURL: "http://api.giphy.com/v1/gifs/search?q=" + gipheed.theVars.query + "&api_key=CukVvGuLlYmHG7AfCRA2mnKRIiGe1XoN&limit=" + gipheed.theVars.limit.toString() + "&rating=" + gipheed.theVars.rating,
-        query: "",
-        limit: 5,
-        rating: "",
-        ratingArray: ["Y","G","PG","PG-13", "R"],
-
+var theVars = {
+    query: "initial",
+    limit: 5,
+    rating: "G",
+};
+var queryURL = `http://api.giphy.com/v1/gifs/search?q=${theVars.query}&api_key=CukVvGuLlYmHG7AfCRA2mnKRIiGe1XoN&limit=${theVars.limit.toString()}&rating=${theVars.rating}`;
+var theFuncs = {
+    makeApiRequest: function() {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function(response) {
+            console.log(response);
+        });
     },
-    theFuncs = {
-        makeApiRequest = function() {
-            // make a request of the api. this function will be called when a button gets clicked on
-        },
-        theClicks = function() {
-            // on click event listeners
-        },
+    theClicks: function() {
+        $(document).on("click", ".btn", function() {
+            theVars.query = $(this).attr("data-reaction");
+            theFuncs.makeApiRequest();
+        });
     },
-}
+};
+theFuncs.theClicks();
