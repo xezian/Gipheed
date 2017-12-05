79fd447420d0785e24f9912f7c5077d6e1b70ab6
xezian <xezian@icloud.com>
2017-12-04 19:24:17 -0700

    some very beginning JavaScripting

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+// this is an app which will send a request to the GIPHY API based on which button the user pressed to return an assortment of GIFs to to the display for the user to enjoy looking at, and which can be held still or paused by clicking
+var gipheed = {
+    theVars = {
+        queryURL: "http://api.giphy.com/v1/gifs/search?q=" + gipheed.theVars.query + "&api_key=CukVvGuLlYmHG7AfCRA2mnKRIiGe1XoN&limit=" + gipheed.theVars.limit.toString() + "&rating=" + gipheed.theVars.rating,
+        query: "",
+        limit: 5,
+        rating: "",
+        ratingArray: ["Y","G","PG","PG-13", "R"],
+
+    },
+    theFuncs = {
+        createDivs = function() {
+            // declare all the divs
+            // give them ids and classes
+            // append them to the body 
+        },
+        makeApiRequest = function() {
+            // make a request of the api. this function will be called when a button gets clicked on
+        },
+        theClicks = function() {
+            // on click event listeners
+        },
+    },
+}
