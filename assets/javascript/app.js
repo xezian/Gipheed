a4d6c3626c0ab55562fadf814261b3e7223a66b1
xezian <xezian@icloud.com>
2017-12-06 22:19:19 -0700

    app is fully functional rough draft ready for fine styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,15 +18,20 @@ var theVars = {
         }
     }
 };
+// select a random bootstrap color
+// TODO change to random css colors
 var randoColor = function() {
     return theVars.colorChoices[Math.floor(Math.random() * theVars.colorChoices.length)];
 };
+// returns query url when performing normal search
 var querySearchURL = function(query, limit, rating) {
     return `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=CukVvGuLlYmHG7AfCRA2mnKRIiGe1XoN&limit=${limit}&rating=${rating}`;
 } 
+// returns query url when performing random search
 var queryRandomURL = function(query, rating) {
     return `http://api.giphy.com/v1/gifs/random?api_key=CukVvGuLlYmHG7AfCRA2mnKRIiGe1XoN&tag=${query}&rating=${rating}`;
 } 
+// gives the gif to an html element and sticks it to the page
 var displaySomeBeauty = function () {    
     var somethingToSee = $("<img/>");
     somethingToSee
@@ -37,35 +42,40 @@ var displaySomeBeauty = function () {
         .appendTo("#giph-area");
 }
 var makeApiRequest = function() {
+    // if random is selected
     if (theVars.random()) {
         var queryURL = queryRandomURL(theVars.query, theVars.rating());
         console.log(queryURL);
+        // we must make an api call per number set as limit because random only returns one gif at a time
         for (var i = 0; i < parseInt(theVars.limit()); i++) {
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).done(function(response) {
                 console.log(response);
-                giphURL = response.data.fixed_height_small_url;
+                giphURL = response.data.fixed_height_downsampled_url;
                 stillURL = response.data.fixed_height_small_still_url;
                 displaySomeBeauty();
             });
         }
-    } else { 
+    } else {
+        // here only one api call is needed for a regular search 
         var queryURL = querySearchURL(theVars.query, theVars.limit(), theVars.rating());
         $.ajax({
             url: queryURL,
             method: "GET"
         }).done(function(response) {
             console.log(response);
+            // now we go through the array inside the response object which should be the length limit was set to
             for (var i = 0; i < response.data.length; i++) {
-                giphURL = response.data[i].images.fixed_height_small.url;
+                giphURL = response.data[i].images.fixed_height.url;
                 stillURL = response.data[i].images.fixed_height_small_still.url;
                 displaySomeBeauty();
             }
         });
     }
 };
+// sweet little function here to handle on click stuff
 var theClicks = function() {
     $(document).on("click", ".btnz", function() {
         theVars.query = $(this).attr("data-reaction");
@@ -93,5 +103,7 @@ var theClicks = function() {
         }
     });
 };
-
-theClicks();
+// let's call that inside the document ready state
+$(document).ready(function () {
+    theClicks();
+});
