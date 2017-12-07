58954f540fe94f8034229eec065f3445699e9566
xezian <xezian@icloud.com>
2017-12-07 13:44:45 -0700

    added some font styling, random button colors, font awesome icons that spin, and script to make it go both ways

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,9 @@ var theVars = {
     query: "",
     giphURL: "",
     stillURL: "",
-    colorChoices: ["primary", "success", "info", "warning", "danger", "inverse", "faded", "light", "dark"],
+    colorChoices: ["pink", "orange", "green", "yellow", "red", "white", "blue", "turquise", "black"],
+    leftIsOpen: false,
+    rightIsOpen: false,
     limit: function() {
         return $("#limit-choice").val();
     },
@@ -23,14 +25,17 @@ var theVars = {
 var randoColor = function() {
     return theVars.colorChoices[Math.floor(Math.random() * theVars.colorChoices.length)];
 };
+var randoExistingButtons = function () {
+    $(".btnz").addClass(randoColor);
+};
 // returns query url when performing normal search
 var querySearchURL = function(query, limit, rating) {
     return `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=CukVvGuLlYmHG7AfCRA2mnKRIiGe1XoN&limit=${limit}&rating=${rating}`;
-} 
+};
 // returns query url when performing random search
 var queryRandomURL = function(query, rating) {
     return `http://api.giphy.com/v1/gifs/random?api_key=CukVvGuLlYmHG7AfCRA2mnKRIiGe1XoN&tag=${query}&rating=${rating}`;
-} 
+};
 // gives the gif to an html element and sticks it to the page
 var displaySomeBeauty = function () {    
     var somethingToSee = $("<img/>");
@@ -40,7 +45,7 @@ var displaySomeBeauty = function () {
         .attr("data-still", stillURL)
         .attr("data-giph", giphURL)
         .appendTo("#giph-area");
-}
+};
 var makeApiRequest = function() {
     // if random is selected
     if (theVars.random()) {
@@ -75,6 +80,47 @@ var makeApiRequest = function() {
         });
     }
 };
+var makeNewButton = function () {
+    var newButton = $("<button>");
+    var inputField = $("#what-button").val().trim();
+    // this clears the form when a new button is submitted
+    $("#submitButtonForm").find("input:text").val("");
+    if (inputField === "") {
+        return
+    } else {
+        newButton
+            .attr("class", `btnz btn ${randoColor()}`)
+            .attr("id", inputField)
+            .attr("data-reaction", inputField.replace(/\s/g,'+'))
+            .text(inputField)
+            .appendTo($("#button-party"));
+    }
+};
+// function to spin the font awesome symbol 
+var faSpin = function (icon) {
+    icon
+        .removeClass("fa")
+        .addClass("fas")
+        .addClass("fa-spin");
+    setTimeout(function(){
+        icon
+            .removeClass("fa")
+            .removeClass("fa-spin")
+            .addClass("fas");
+    }, 2000);
+}
+var faSpinReverse = function(icon) {
+    icon
+        .removeClass("fa")
+        .addClass("fas")
+        .addClass("fa-spin-reverse");
+    setTimeout(function(){
+        icon
+            .removeClass("fa")
+            .removeClass("fa-spin-reverse")
+            .addClass("fas");
+    }, 2000);
+}
 // sweet little function here to handle on click stuff
 var theClicks = function() {
     $(document).on("click", ".btnz", function() {
@@ -82,15 +128,14 @@ var theClicks = function() {
         $("#giph-area").empty();
         makeApiRequest();
     });
+    // not a click technically but here's a keyup listener for the enter key
+    $("#what-button").keypress(function(e) {
+        if (e.which == 13)  {
+            makeNewButton();
+        }
+    });
     $(document).on("click", "#new-button", function() {
-        var newButton = $("<button>");
-        var inputField = $("#what-button").val().trim();
-        newButton
-            .attr("class", `btnz btn btn-${randoColor()}`)
-            .attr("id", inputField)
-            .attr("data-reaction", inputField.replace(/\s/g,'+'))
-            .text(inputField)
-            .appendTo($("#button-party"));
+        makeNewButton();
     });
     $(document).on("click", ".gipher", function() {
         var gipher = $(this);
@@ -102,8 +147,29 @@ var theClicks = function() {
             console.log("problem!")
         }
     });
+    $(document).on("click", "#fa-edit", function() {
+        if (theVars.leftIsOpen) {
+            faSpinReverse($(this));
+            theVars.leftIsOpen = false;
+        } 
+        else {
+            faSpin($(this));
+            theVars.leftIsOpen = true;
+        }
+    });
+    $(document).on("click", "#fa-add", function() {
+        if (theVars.rightIsOpen) {
+            faSpin($(this));
+            theVars.rightIsOpen = false;
+        } 
+        else {
+            faSpinReverse($(this));
+            theVars.rightIsOpen = true;
+        }
+    });
 };
 // let's call that inside the document ready state
 $(document).ready(function () {
     theClicks();
+    randoExistingButtons();
 });
